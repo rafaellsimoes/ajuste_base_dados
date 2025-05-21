bf21bf0960b37e20b947104025e3904d1c2c1029
Rafael <48397061+rafaellsimoes@users.noreply.github.com>
2025-05-21 06:31:04 -0400

    Update Dados_Siope.py

--- Dados_Siope.py
+++ Dados_Siope.py
@@ -34,12 +34,89 @@ for year in range(2021, 2025):
     
     response = requests.get(url)
     if response.status_code == 200:
+         dados = response.json()
+    if dados.get('value'):
+           for item in dados['value']:
+               item['ANO'] = year
+           dados_totais.extend(dados['value'])
+
+dados_totais_ac = pd.DataFrame(dados_totais)
+dados_totais_ac.to_csv(r'C:\Users\orafa\OneDrive\Documents\dados impopulares\Dados Sociais\Educação + Judiciário\Dados SIOPE - ACRE\receitas_acre_siope.csv', index=False)
+dados_totais_ac.to_excel(r'C:\Users\orafa\OneDrive\Documents\dados impopulares\Dados Sociais\Educação + Judiciário\Dados SIOPE - ACRE\receitas_acre_siope.xlsx', index=False)
+
+
+# dados Amazonas 
+
+dados_totais = []
+
+for year in range(2021, 2025):
+    url = (f"https://www.fnde.gov.br/olinda-ide/servico/DADOS_ABERTOS_SIOPE/versao/v1/odata/"
+           f"Despesas_Funcao_Educacao_Siope(Ano_Consulta=@Ano_Consulta,Num_Peri=@Num_Peri,Sig_UF=@Sig_UF)"
+           f"?@Ano_Consulta={year}&@Num_Peri=1&@Sig_UF='AM'&$top=10000"
+           f"&$filter=TIPO%20eq%20'Estadual'"
+           f"&$format=json"
+           f"&$select=TIPO,NUM_ANO,NUM_PERI,COD_UF,SIG_UF,COD_MUNI,NOM_MUNI,NUM_ORDE,DES_SUBF,VAL_DESP_EMPE,VAL_DESP_LIQU,VAL_DESP_PAGA")
+    
+    response = requests.get(url)
+    if response.status_code == 200:
         dados = response.json()
         if dados.get('value'):
             for item in dados['value']:
                 item['ANO'] = year
             dados_totais.extend(dados['value'])
 
-dados_totais_ac = pd.DataFrame(dados_totais)
-dados_totais_ac.to_csv(r'C:\Users\orafa\OneDrive\Documents\dados impopulares\Dados Sociais\Educação + Judiciário\Dados SIOPE - ACRE\receitas_acre_siope.csv', index=False)
-dados_totais_ac.to_excel(r'C:\Users\orafa\OneDrive\Documents\dados impopulares\Dados Sociais\Educação + Judiciário\Dados SIOPE - ACRE\receitas_acre_siope.xlsx', index=False)
+dados_receitas_am_despesas = pd.DataFrame(dados_totais) 
+dados_receitas_am_despesas.to_csv(r'C:\Users\orafa\OneDrive\Documents\dados impopulares\Dados Sociais\Educação + Judiciário\Dados SIOPE - ACRE\despesas_amazonas_siope.csv',index=False)
+dados_receitas_am_despesas.to_excel(r'C:\Users\orafa\OneDrive\Documents\dados impopulares\Dados Sociais\Educação + Judiciário\Dados SIOPE - ACRE\despesas_amazonas_siope.xlsx',index=False)
+
+print(dados_receitas_am_despesas.head())
+
+dados_totais = []
+for year in range(2021, 2025):
+    url = (f"https://www.fnde.gov.br/olinda-ide/servico/DADOS_ABERTOS_SIOPE/versao/v1/odata/"
+          f"Receita_Siope(Ano_Consulta=@Ano_Consulta,Num_Peri=@Num_Peri,Sig_UF=@Sig_UF)"
+          f"?@Ano_Consulta={year}&@Num_Peri=1&@Sig_UF='AM'&$top=10000"
+          f"&$filter=TIPO%20eq%20'Estadual'"
+          f"&$format=json"
+          f"&$select=TIPO,NUM_ANO,NUM_PERI,COD_UF,SIG_UF,COD_MUNI,NOM_MUNI,COD_EXIB_FORMATADO,"
+          f"NOM_ITEM,IDN_CLAS,NOM_COLU,NUM_NIVE,NUM_ORDE,VAL_DECL")
+    
+    response = requests.get(url)
+    if response.status_code == 200:
+        dados = response.json()
+        if dados.get('value'):
+            for item in dados['value']:
+                item['ANO'] = year
+            dados_totais.extend(dados['value'])
+            
+dados_despesas_am = pd.DataFrame(dados_totais) 
+print(dados_despesas_am.head())
+
+dados_despesas_am.to_csv(r'C:\Users\orafa\OneDrive\Documents\dados impopulares\Dados Sociais\Educação + Judiciário\Dados SIOPE - ACRE\receitas_amazonas_siope.csv',index=False)
+dados_despesas_am.to_excel(r'C:\Users\orafa\OneDrive\Documents\dados impopulares\Dados Sociais\Educação + Judiciário\Dados SIOPE - ACRE\receitas_amazonas_siope.xlsx',index=False)          
+print(dados_despesas_am.head())
+
+Dados Roraima
+
+dados_totais = []
+
+for year in range(2019, 2025):
+    url = (f"https://www.fnde.gov.br/olinda-ide/servico/DADOS_ABERTOS_SIOPE/versao/v1/odata/"
+           f"Despesas_Funcao_Educacao_Siope(Ano_Consulta=@Ano_Consulta,Num_Peri=@Num_Peri,Sig_UF=@Sig_UF)"
+           f"?@Ano_Consulta={year}&@Num_Peri=1&@Sig_UF='RR'&$top=10000"
+           f"&$filter=TIPO%20eq%20'Estadual'"
+           f"&$format=json"
+           f"&$select=TIPO,NUM_ANO,NUM_PERI,COD_UF,SIG_UF,COD_MUNI,NOM_MUNI,NUM_ORDE,DES_SUBF,VAL_DESP_EMPE,VAL_DESP_LIQU,VAL_DESP_PAGA")
+    
+    response = requests.get(url)
+    if response.status_code == 200:
+        dados = response.json()
+        if dados.get('value'):
+            for i in dados['value']:
+                dados['ANO'] = year
+            dados_totais.extend(dados['value'])
+            
+dados_despesas_rr = pd.DataFrame(dados_totais) 
+print(dados_despesas_rr)
+
+        
